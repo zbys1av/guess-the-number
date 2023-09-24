4e8acafe3195022e4557e69e0aeffa4cc5289731
zbys1av <zbyslav13@gmail.com>
2023-09-24 20:28:06 +0300

    main functions added

--- src/index.js
+++ src/index.js
@@ -3,66 +3,52 @@ import './style.scss';
 'use strict';
 document.addEventListener('DOMContentLoaded', ()=>{
 
-    const confirmButton = document.getElementById("btn");
-    const inputField = document.getElementById("user-input");
+    // const inputField = document.getElementById("user-input");
     const correctNumberBtn = document.getElementById("correct");
+    const higherBtn = document.getElementById("higher");
+    const lowerBtn = document.getElementById("lower");
+
     let numberText = document.getElementById("main__number");
     let questionText = document.getElementById("main__question");
     let correctAnswer = document.getElementById("correct-answer");
 
-    let minNumber = 0;
-    let maxNumber = 100;
-    let myNumbers = [0, 100];
-    let attempts = 0;
+
+    let numRange = [0, 100];
     let myNumber = Math.round(Math.random() * (80 - 20) + 20);
+    console.log("myNumber: " + myNumber);
 
-    confirmButton.addEventListener("click", test);
+    higherBtn.addEventListener("click", higherNumber);
+    lowerBtn.addEventListener("click", lowerNumber);
 
-
-    console.log("Is your number HIGHER/LOWER than " + myNumber + "?");
     numberText.textContent  = "Is your number " + myNumber + "?";
-    questionText.innerHTML  = "Or it is higher(+) or lower(-)?";
+    questionText.innerHTML  = "Or it is higher or lower?";
 
-    function test(){
-        if(attempts < 1){
-            if(inputField.value == "+"){
-                myNumber = Math.round(myNumber*1.5);
-                console.log(myNumber);
-            } else if (inputField.value == "-"){
-                myNumber = Math.round(myNumber/2);
-                console.log(myNumber);
-            } else {
-                console.log("VICTORY!!!");
-            }
-        } else {
-            if(inputField.value == "+"){
-                myNumbers[1] = Math.round(myNumber*1.5);
-                console.log("MAX " + myNumbers[1]);
-            } else if (inputField.value == "-"){
-                myNumbers[0] = Math.round(myNumber/2);
-                console.log("MIN " + myNumbers[0]);
-            } else {
-                console.log("VICTORY!!!");
-            }
-            myNumber = Math.round((myNumbers[0]+myNumbers[1])/2);
-            console.log("MIDDLE: " + myNumber);
-        }
+    function higherNumber(){
+        numRange[0] = myNumber;
+        myNumber = Math.round((numRange[0]+numRange[1])/2);
+        console.log("numRANGE: " + numRange);
+        console.log("myNumber: " + myNumber);
 
-
-        attempts += 1;
-        console.log("Attempts: " + attempts);
-
-        console.log(inputField.value);
-        inputField.value = "";
-
-        console.log("Is your number HIGHER/LOWER than " + myNumber + "?");
-        numberText.textContent  = "Is your number " + myNumber + "?";
-        questionText.innerHTML  = "Or it is higher(+) or lower(-)?";
-
+        findNumber();
     }
 
-    correctNumberBtn.addEventListener("click", correct);
+    function lowerNumber(){
+        numRange[1] = myNumber;
+        myNumber = Math.round((numRange[0]+numRange[1])/2);
+        console.log("numRANGE: " + numRange);
+        console.log("myNumber: " + myNumber);
 
+        findNumber();
+    }
+
+    function findNumber(){
+        numberText.textContent  = "Is your number " + myNumber + "?";
+        questionText.innerHTML  = "Or it is higher or lower?";
+    }
+
+
+    correctNumberBtn.addEventListener("click", correct);
+    
     function correct(){
         correctAnswer.textContent  = "💛";
     }
